8e4f3935c8ce9cf098b598c259c5f98273a68aca
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-08-21 15:32:08 +0200

    Add binarySearchWith

--- src/Data/BTree/Array/BinarySearch.hs
+++ src/Data/BTree/Array/BinarySearch.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE BangPatterns #-}
 module Data.BTree.Array.BinarySearch
     ( binarySearch
+    , binarySearchWith
     ) where
 
 import Data.BTree.Array (Array)
@@ -14,13 +15,40 @@ binarySearch :: Ord a
              -> Maybe Int   -- ^ Result
 binarySearch size x arr = go 0 (size - 1)
   where
-    go !lo !up
-        | lo > up   = Nothing
+    go !lo !hi
+        | lo > hi   = Nothing
         | otherwise =
-            let !guess = (lo + up) `div` 2
+            let !guess = (lo + hi) `div` 2
                 !y     = A.unsafeIndex arr guess
             in case compare x y of
                 LT -> go lo (guess - 1)
-                GT -> go (guess + 1) up
+                GT -> go (guess + 1) hi
                 EQ -> Just guess
 {-# INLINE binarySearch #-}
+
+binarySearchWith :: Ord a
+                 => (Int -> b)  -- ^ Found; pass the index
+                 -> (Int -> b)  -- ^ Not found; pass the first higher index
+                 -> Int         -- ^ Size of the array to search
+                 -> a           -- ^ Item to search
+                 -> Array a     -- ^ Array to search
+                 -> b           -- ^ Result
+binarySearchWith found notFound size x arr = go 0 size
+  where
+    go !lo !hi
+        | lo + 1 >= hi =
+            if lo >= size
+                then notFound lo
+                else let !y = A.unsafeIndex arr lo
+                     in case compare x y of
+                         EQ -> found lo
+                         LT -> notFound lo
+                         GT -> notFound (lo + 1)
+        | otherwise    =
+            let !guess = (lo + hi) `div` 2
+                !y     = A.unsafeIndex arr guess
+            in case compare x y of
+                LT -> go lo guess
+                GT -> go guess hi
+                EQ -> found guess
+{-# INLINE binarySearchWith #-}
--- tests/Data/BTree/Array/BinarySearch/Tests.hs
+++ tests/Data/BTree/Array/BinarySearch/Tests.hs
@@ -12,7 +12,8 @@ import qualified Data.BTree.Array as A
 
 tests :: Test
 tests = testGroup "Data.BTree.Array.BinarySearch.Tests"
-    [ testProperty "binarySearch" t_binarySearch
+    [ testProperty "binarySearch"     t_binarySearch
+    , testProperty "binarySearchWith" t_binarySearchWith
     ]
 
 t_binarySearch :: [Int] -> Int -> Bool
@@ -23,3 +24,16 @@ t_binarySearch list x = case binarySearch length' x array of
     sorted  = sort $ nub list
     length' = length sorted
     array   = A.fromList sorted
+
+t_binarySearchWith :: [Int] -> Int -> Bool
+t_binarySearchWith list x = case binarySearchWith Left Right length' x array of
+    Left i  -> sorted !! i == x
+    Right i -> x `notElem` sorted &&
+        bound i (sorted !! i > x) &&
+        bound (i - 1) (sorted !! (i - 1) < x)
+  where
+    sorted  = sort $ nub list
+    length' = length sorted
+    array   = A.fromList sorted
+
+    bound i e = if i >= 0 && i < length' then e else True
